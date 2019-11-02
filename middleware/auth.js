2bb5dcb0c5805003f204ebe55ccc6c7295c06607
garabavlad <garaba.vlad@Vladislavs-MacBook-Pro.local>
2019-11-02 20:22:15 +0200

    backend done; installed react app

---
+++ middleware/auth.js
@@ -0,0 +1,18 @@
+const jwt = require('jsonwebtoken');
+const config = require('config');
+
+module.exports = function(req, res, next) {
+	const token = req.header('x-auth-token');
+	if (!token) {
+		return res.status(401).json({ msg: 'No a token found' });
+	}
+
+	try {
+		const verification = jwt.verify(token, config.get('jwtSecret'));
+
+		req.user = verification.user;
+		next();
+	} catch (error) {
+		return res.status(401).json({ msg: 'Invalid token bro' });
+	}
+};
